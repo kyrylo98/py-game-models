c2ab6c0646d5f8c078d71488010d1158583c331b
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-06-24 16:45:41 +0200

    Create players

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from db.models import Race, Skill, Player, Guild
 
 def main() -> None:
     with open("players.json", "r", encoding="utf-8") as file:
-        players = json.load(file)
+        players_data = json.load(file)
 
-    for player in players:
+    for player_name, player in players_data.items():
         race, _ = Race.objects.get_or_create(
             name=player["race"]["name"],
             defaults={"description": player["race"]["description"]}
         )
 
         guild = None
-        if player.get("guild"):
+        if player["guild"]:
             guild, _ = Guild.objects.get_or_create(
                 name=player["guild"]["name"],
                 defaults={"description": player["guild"]["description"]}
             )
 
-        for skill_data in player["skills"]:
+        for skill_data in player["race"]["skills"]:
             Skill.objects.get_or_create(
                 name=skill_data["name"],
                 race=race,
@@ -28,7 +28,7 @@ def main() -> None:
             )
 
         Player.objects.create(
-            nickname=player["nickname"],
+            nickname=player_name,
             email=player["email"],
             bio=player["bio"],
             race=race,
