5f0d17209f91018f6291bc1ab18350106bd9a2fa
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-06-24 16:38:20 +0200

    Create players

--- main.py
+++ main.py
@@ -3,37 +3,38 @@ import json
 from db.models import Race, Skill, Player, Guild
 
 
-def main() -> None:
-    with open('players.json', 'r', encoding='utf-8') as file:
+def main():
+    with open("players.json", "r", encoding="utf-8") as file:
         players = json.load(file)
 
     for player in players:
         race, _ = Race.objects.get_or_create(
-            name=player['race']['name'],
-            defaults={'description': player['race']['description']}
+            name=player["race"]["name"],
+            defaults={"description": player["race"]["description"]}
         )
 
         guild = None
-        if player.get('guild'):
+        if player.get("guild"):
             guild, _ = Guild.objects.get_or_create(
-                name=player['guild']['name'],
-                defaults={'description': player['guild']['description']}
+                name=player["guild"]["name"],
+                defaults={"description": player["guild"]["description"]}
             )
 
-        for skill_data in player['skills']:
+        for skill_data in player["skills"]:
             Skill.objects.get_or_create(
-                name=skill_data['name'],
+                name=skill_data["name"],
                 race=race,
-                defaults={'bonus': skill_data['bonus']}
+                defaults={"bonus": skill_data["bonus"]}
             )
 
         Player.objects.create(
-            nickname=player['nickname'],
-            email=player['email'],
-            bio=player['bio'],
+            nickname=player["nickname"],
+            email=player["email"],
+            bio=player["bio"],
             race=race,
             guild=guild
         )
+
 
 if __name__ == "__main__":
     main()
